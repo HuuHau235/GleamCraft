1f24650d6800571462d55913bedf6025d1f014bf
HuuHau235 <hhai98940@gmail.com>
2024-12-08 15:39:21 +0700

    fix header

--- app/views/homepage/index.php
+++ app/views/homepage/index.php
@@ -5,8 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="assets/css/footer.css">
+    <style>
+        .body {
+            background-color: #FFFFE9;
+        }
+        .h2 {
+            color: yellow;
+        }
+    </style>
 </head>
-<body>
-<?php include_once '../shared/header.php'; ?>
-<?php include_once '../shared/footer.php'; ?></body>
+<body class="body">
+<?php
+include_once __DIR__ . '/../shared/header.php';
+include_once __DIR__ . '/../shared/footer.php';
+?>
+</body>
 </html>
