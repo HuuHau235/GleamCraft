58fa1d2a81956f2897a9594cf5cf986b148e3c0a
KimThanh1801 <hokimthanh1234@gmail.com>
2024-12-12 14:07:10 +0700

    thêm login.js

--- app/models/Product.php
+++ app/models/Product.php
@@ -9,7 +9,7 @@ class Product {
 
     public function __construct() {
         // Kết nối cơ sở dữ liệu
-        $this->db = new PDO('mysql:host=localhost;dbname=gleamcraft', 'root', '1234');
+        $this->db = new PDO('mysql:host=localhost;dbname=gleamcraft', 'root', '');
     }
 
     public function getAllProducts() {
