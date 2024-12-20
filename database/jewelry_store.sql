353adab2c31405f815b25ea9608b03c61d437b19
KimThanh1801 <hokimthanh1234@gmail.com>
2024-12-20 15:07:52 +0700

    Save local changes

--- database/jewelry_store.sql
+++ database/jewelry_store.sql
@@ -27,7 +27,17 @@ CREATE TABLE Products (
     price DECIMAL(10, 2),
     image VARCHAR(255)
 );
-
+--  Table : Cart
+CREATE TABLE cart (
+    cart_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    product_name VARCHAR(255) DEFAULT NULL,
+    product_image VARCHAR(255) DEFAULT NULL,
+    product_description TEXT DEFAULT NULL,
+    product_price DECIMAL(10,2) DEFAULT NULL,
+    INDEX product_idx (product_id)
+);
 -- Table: Orders
 DROP TABLE IF EXISTS Orders;
 CREATE TABLE Orders (
