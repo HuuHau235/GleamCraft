12c3eccbd3c0586c6c01a70d87510ba2229ae1cd
HuuHau235 <hhai98940@gmail.com>
2024-12-26 19:55:33 +0700

    change link title

--- app/views/product/index.php
+++ app/views/product/index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-    <link rel="icon" href="../assets/images/brands/logo.jpg" type="image/x-icon">
+    <link rel="icon" href="../../assets/images/brands/logo.jpg" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="../../assets/css/header.css">
     <!-- <base href="http://localhost/GleamCraft/"> -->
     <!-- <link rel="stylesheet" href="/GleamCraft/assets/css/product.css"> -->
-    <title>Products</title>
+    <title>GleamCraft</title>
     <style>
     body {
         font-family: 'Source Sans Pro', sans-serif;
